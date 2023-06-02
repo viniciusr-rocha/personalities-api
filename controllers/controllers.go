5c45ee6fc7217078b83104cfc23bd96135fae271
Vinicius Rocha <vinicius.rodrigss@gmail.com>
2023-06-02 18:49:15 -0300

    feat: Retornando recurso por ID

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"personalities-api/models"
+	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +15,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, personalidade := range models.Personalidades {
+		if strconv.Itoa(personalidade.Id) == id {
+			json.NewEncoder(w).Encode(personalidade)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	models.Personalidades = []models.Personalidade{
-		{"nome1", "historia 1"},
-		{"nome2", "historia 2"},
-		{"nome3", "historia 3"},
+		{1, "nome1", "historia 1"},
+		{2, "nome2", "historia 2"},
+		{3, "nome3", "historia 3"},
 	}
 
 	fmt.Println("Iniciando servidor...")
