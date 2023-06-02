c77c5a33dc1b3aa6fbb92bf63ff0088b5893c6f2
Vinicius Rocha <vinicius.rodrigss@gmail.com>
2023-06-02 18:41:08 -0300

    chore: Adicionado novo router gorilla/mux

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,15 @@
 package routes
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"personalities-api/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
